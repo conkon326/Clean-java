1bd1fab1f4ec2e8a096257c892dc6ea2877074a5
con <128914799+conkon326@users.noreply.github.com>
2023-07-31 11:29:44 +0900

    Add files via upload

---
+++ chap4/Codo4_7.java
@@ -0,0 +1,12 @@
+public class Codo4_7 {
+
+  public static void main(String[] args) {
+    int[] scores;
+    scores = new int[4];
+    int num = scores.length;
+      System.out.println(num);
+    for (int i = 0; i < scores.length; i++) {
+      System.out.println(scores[i] + "," + i + ",");
+    
+  }
+}}
