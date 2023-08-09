84dc22ce31fa9830328a5c6ff1091fee5a65ff12
con <128914799+conkon326@users.noreply.github.com>
2023-08-09 13:38:36 +0900

    q

--- Q/Q1.java
+++ Q/Q1.java
@@ -1,8 +1,8 @@
 public class Q1 {
     public static void main(String[] args) {
         long num = 1470268800;
-        int lowLimit = 10000;
-        int upLimit = 99999;
+        int lowLimit = 10000;// スタート
+        int upLimit = 99999;// ラスト
 
         long sum = 0;
 
@@ -15,3 +15,7 @@ public class Q1 {
         System.out.println(sum);
     }
 }
+
+// 約数をひたすら足していく
+// 1470268800の正の約数のうち、
+// 10000 以上 99999 以下の範囲にあるものを全て足し合わせたときの和を求めてください。
